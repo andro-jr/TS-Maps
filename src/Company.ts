4cda2265c7bf55b7af164586565f8c6c958105e2
andro-jr <prabin20panta@gmail.com>
2024-04-23 12:26:26 +0545

    google maps integration

---
+++ src/Company.ts
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker';
+
+export class Company {
+  name: string;
+  phrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.company.name();
+    this.phrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import { Company } from './Company';
 import { User } from './User';
+
+const user = new User();
+const company = new Company();
+console.log('user :', user);
+console.log('company :', company);
